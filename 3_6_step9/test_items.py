ae59be92f9ace7fd055a3b8d0b0fe096b6f1dc8a
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-03 22:04:15 +0300

    Финальный допил.

--- 3_6_step9/test_items.py
+++ 3_6_step9/test_items.py
@@ -1,10 +1,8 @@
 from selenium.webdriver.common.by import By
 import time
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-
 def test_guest_should_see_basket_form(browser):
     browser.get(link)
     time.sleep(30)
-    message = browser.find_element(By.ID, "add_to_basket_form")
-    # Проверка наличия кнопки
-    assert "Добавить в корзину" in message.text
+    button = browser.find_elements(By.XPATH, "//*[@id='add_to_basket_form']/button")
+    assert button, "Кнопка не найдена"
