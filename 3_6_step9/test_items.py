27ce369a0897f895251844977976e1a75206e886
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-03 20:22:28 +0300

    Изменено название функции.

--- 3_6_step9/test_items.py
+++ 3_6_step9/test_items.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
-def test_guest_should_see_login_link(browser):
+def test_guest_should_see_basket_form(browser):
     browser.get(link)
     message = browser.find_element(By.ID, "add_to_basket_form")
     # Проверка наличия кнопки
