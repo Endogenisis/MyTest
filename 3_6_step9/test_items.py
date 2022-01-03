ff297e8418f31509368ed4d9dd31e4ab621075be
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-03 20:44:35 +0300

    Добавлен слип.

--- 3_6_step9/test_items.py
+++ 3_6_step9/test_items.py
@@ -1,8 +1,10 @@
 from selenium.webdriver.common.by import By
+import time
 link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 def test_guest_should_see_basket_form(browser):
     browser.get(link)
+    time.sleep(30)
     message = browser.find_element(By.ID, "add_to_basket_form")
     # Проверка наличия кнопки
     assert "Добавить в корзину" in message.text
