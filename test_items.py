760540b753ca51b041d04220a411aae839b0871d
Endogenisis <96134799+Endogenisis@users.noreply.github.com>
2022-01-03 13:41:37 +0300

    Файлы теста

---
+++ test_items.py
@@ -0,0 +1,6 @@
+from selenium.webdriver.common.by import By
+link = " http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_guest_should_see_login_link(browser):
+    browser.get(link)
+    browser.find_element(By.ID, "add_to_basket_form")
